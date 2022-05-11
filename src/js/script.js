21e50decbb141df2964708a98d688f4dc3a26fdc
max_air <max_air@bk.ru>
2022-05-11 14:51:12 +0300

    Слайдер сделан Slick слайдер

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function () {
-    $('.courusel__inner').slick();
+    $('.courusel__inner').slick({
+        speed: 600,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/chevron_left.png" alt="slide"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/chevron_right.png" alt="slide"></button>',
+        // prevArrow: '<img class="slick-prev" src="../img/slider/chevron_left.png" alt="slide">',
+        // nextArrow: '<img class="slick-next" src="../img/slider/chevron_right.png" alt="slide">',
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    dots: true,
+                    slidesToShow: 1,
+                    autoplay: false
+
+                }
+            }
+        ]
+    });
 });
+
